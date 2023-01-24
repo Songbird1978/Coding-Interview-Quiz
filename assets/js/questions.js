81217c005602e02bef514860afc510c00afe4e8b
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-01-24 15:52:00 +0000

    added questions and empty javascript files

---
+++ assets/js/questions.js
@@ -0,0 +1,49 @@
+const questionsData = [
+
+{
+question: "1. What is scope in Javascript?",
+
+a:  "How long it takes to learn Javascript.",
+b:  "A collection of rules for how variables are accessed in relation to the function itself.",
+c:  "The best practices implemented in your code.",
+d:  "How accessible your code is for other developers.",
+correct: "b",
+}, 
+
+{
+question: "2. What does the DOM stand for?",
+a:  "Document Object Model",
+b:  "Document Object Module",
+c:  "Document Open Mount",
+d:  "Download Object Model",
+correct: "a",
+}, 
+
+{
+question: "3. Select the option, which does not contain a JavaScript array?",
+a:  "const arr1 = [1, 2, 3, 4];",
+b:  "const arr1 = '1, 2, 3, 4';",
+c:  "const arr1 = ['1', '2', '3', '4'];",
+d:  "const arr1 = [1, 2, '3', '4'];",
+correct: "b",
+}, 
+
+{
+question: "4. Where is the error in the given Javascript loop?<br><br>const arr = ['April', 'May', 'June'];<br> for (let counter = 0; counter < arr.length; i++){ <br> console.log(arr[counter]);<br> };",
+a:  "The keyword for is an invalid keyword.",
+b:  "The variable i should be replaced with counter.",
+c:  "The variable arr cannot be iterated.",
+d:  "The variable counter should be initialized with 1.",
+correct: "b",
+}, 
+
+{
+question: "5. Math.random() is ...",
+a:  "A method that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.",
+b:  "A function that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.",
+c:  "A method that applies a random equation to a number.",
+d:  "A function that chooses a random number and multiplies it by 100.",
+correct: "a",
+},
+
+];
