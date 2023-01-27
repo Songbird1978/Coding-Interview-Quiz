74632b22cae3649b51c7d8fdd68c6e16c0df3bfe
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-01-27 17:45:49 +0000

    questions run through to end of quiz. needs link to end message, scores, timer and local storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,12 +2,13 @@ let startQuiz = document.querySelector("#start"); //linking new variable to the 
 let questionBox = document.querySelector("#questions"); //linking new variable to the id of questions (html)
 let question = document.querySelector("#question-title"); //linking new variable to the id of question-title 
 let choiceBox = document.querySelector("#choices"); //linking new variable to the id of choices
+let endQuiz = document.querySelector("#end-screen"); //linking new variable to the end screen
+let score = 0;
+let clickListener = 0;
 
-var continueQuiz = function (questionIndex) {
+var continueQuiz = function (questionIndex) { //continueQuiz is a function that works with the questionData array (index)
 
-
-
-    question.textContent = questionData[questionIndex].question //defining the element question with a property of textContent as the questionData starting at the 1st element of the array 
+    question.textContent = questionData[questionIndex].question; //defining the element question with a property of textContent as the questionData starting at the 1st element of the array 
 
     const list = document.createElement("ul"); // creating a new unordered list element in the html - the parent element - the question
 
@@ -19,33 +20,71 @@ var continueQuiz = function (questionIndex) {
         button.textContent = questionData[questionIndex].choices[i]; //linking the text of the question (questionData array) and the choices div to the answer buttons 
         li.appendChild(button); //appending the button as a child of the ul
         button.setAttribute('id', 'answerbutton');//setting an id of answer for the answer buttons 
-        console.log(button); //console log of button to check the attribute is set
+
 
         button.addEventListener("click", function () { //add function for click answer
-            if (questionData[questionIndex].choices[i] === questionData[questionIndex].correct) {
-                console.log('yes');
+            if (questionData[questionIndex].choices[i] === questionData[questionIndex].correct) { //requires the choice to be equal to the correct answer
+                console.log('correct');
+                score++;
+                clickListener++;
+                console.log("score = " + score);
+                alert("That's correct!"); //
 
             } else {
-                console.log('no');
+                console.log('incorrect'); //if it is not - it is incorrect 
+                alert("That's incorrect!");
+                clickListener++;
+                console.log("score = " + score);
+
+            } if (clickListener === questionData.length) {
+
+                alert("You got " + score + " out of " + questionData.length);
+        
+                console.log("end");
+
+                endQuiz();
             };
-            choiceBox.removeChild(list);
-            continueQuiz(questionIndex + 1);
-        })
+        
+
+            choiceBox.removeChild(list); //removes the question that was previously displayed
+            continueQuiz(questionIndex + 1); //replaces with the next question in the index of questions   
+
+        });
     };
+
 
     choiceBox.appendChild(list); //appending the choiceBox (choices) to the list element 
 
+    
+
 }
+
+
+
+
 
 startQuiz.addEventListener("click", function () { //creating an event listener for the click which runs a function
 
     questionBox.classList.remove("hide") //removing the class list property of hide - showing the questions to be answered
 
-    continueQuiz(0);
-
+    continueQuiz(0); //runs the first function startQuiz
 
 });
 
+
+endQuiz = function () {
+
+
+    questionBox.classList.add("hide") //removing the class list property of hide - showing the end screen
+    //choiceBox.removeChild(list);
+    console.log("endquiz");
+
+};
+
+
+var startTimer = function(){
+    
+};
 
 
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,49 +1,34 @@
 const questionData = [
 
-{
-question: "1. What is scope in Javascript?",
-choices : [ "How long it takes to learn Javascript.", "A collection of rules for how variables are accessed in relation to the function itself.", "The best practices implemented in your code.", "How accessible your code is for other developers." ],
-correct: "A collection of rules for how variables are accessed in relation to the function itself.",
-}, 
+    {
+        question: "1. What is scope in Javascript?",
+        choices: ["How long it takes to learn Javascript.", "A collection of rules for how variables are accessed in relation to the function itself.", "The best practices implemented in your code.", "How accessible your code is for other developers."],
+        correct: "A collection of rules for how variables are accessed in relation to the function itself.",
+    },
 
-{
-question: "2. What does the DOM stand for?",
-choices : [ "Document Object Model", "Document Object Module", "Document Open Mount", "Download Object Model"],
-a:  "Document Object Model",
-b:  "Document Object Module",
-c:  "Document Open Mount",
-d:  "Download Object Model",
-correct: "a",
-}, 
+    {
+        question: "2. What does the DOM stand for?",
+        choices: ["Document Object Model", "Document Object Module", "Document Open Mount", "Download Object Model"],
+        correct: "Document Object Model",
+    },
 
-{
-question: "3. Select the option, which does not contain a JavaScript array?",
-choices: [ "const arr1 = [1, 2, 3, 4];",  "const arr1 = '1, 2, 3, 4';", "const arr1 = ['1', '2', '3', '4'];", "const arr1 = [1, 2, '3', '4'];",],
-a:  "const arr1 = [1, 2, 3, 4];",
-b:  "const arr1 = '1, 2, 3, 4';",
-c:  "const arr1 = ['1', '2', '3', '4'];",
-d:  "const arr1 = [1, 2, '3', '4'];",
-correct: "b",
-}, 
+    {
+        question: "3. Select the option, which does not contain a JavaScript array?",
+        choices: ["const arr1 = [1, 2, 3, 4];", "const arr1 = '1, 2, 3, 4';", "const arr1 = ['1', '2', '3', '4'];", "const arr1 = [1, 2, '3', '4'];",],
+        correct: "const arr1 = '1, 2, 3, 4';",
+    },
 
-{
-question: "4. Where is the error in the given Javascript loop?<br><br>const arr = ['April', 'May', 'June'];<br> for (let counter = 0; counter < arr.length; i++){ <br> console.log(arr[counter]);<br> };",
-choices: ["The keyword for is an invalid keyword.", "The variable i should be replaced with counter.", "The variable arr cannot be iterated.", "The variable counter should be initialized with 1.",],
-a:  "The keyword for is an invalid keyword.",
-b:  "The variable i should be replaced with counter.",
-c:  "The variable arr cannot be iterated.",
-d:  "The variable counter should be initialized with 1.",
-correct: "b",
-}, 
+    {
+        question: '4. Where is the error in the given Javascript loop? \n \ const arr = ["April", "May", "June"]; \n \ for (let counter = 0; counter < arr.length; i++){ \n \ console.log(arr[counter]); \n \ };',
+        choices: ["The keyword for is an invalid keyword.", "The variable i should be replaced with counter.", "The variable arr cannot be iterated.", "The variable counter should be initialized with 1.",],
+        correct: "The variable i should be replaced with counter.",
+    },
 
-{
-question: "5. Math.random() is ...",
-choices: [ "A method that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.", "A function that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.", "A method that applies a random equation to a number.", "A function that chooses a random number and multiplies it by 100.",],
-a:  "A method that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.",
-b:  "A function that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.",
-c:  "A method that applies a random equation to a number.",
-d:  "A function that chooses a random number and multiplies it by 100.",
-correct: "a",
-},
+    {
+        question: "5. Math.random() is ...",
+        choices: ["A method that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.", "A function that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.", "A method that applies a random equation to a number.", "A function that chooses a random number and multiplies it by 100.",],
+        correct: "A method that returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.",
+
+    },
 
 ];
